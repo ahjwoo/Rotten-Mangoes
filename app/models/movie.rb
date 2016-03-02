9f40731eb9b7570f770f6f90f394b0a0ddd4d163
Andrew Woo <ahjwoo@gmail.com>
2016-03-02 09:23:06 +0000

    Seems to be a bug with helper methods...can't get strftime to work. proceeded without.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -10,6 +10,13 @@ class Movie < ActiveRecord::Base
   validates :release_date, presence: true
   validate :release_date_is_in_the_past
 
+  def review_average
+    if reviews.any?
+      reviews.sum(:rating_out_of_ten)/reviews.size
+    end
+  end
+
+
 protected
 
   def release_date_is_in_the_past

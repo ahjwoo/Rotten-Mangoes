8b8535fea2e71b61d283ebfc157dab13cf3d4e00
Andrew Woo <ahjwoo@gmail.com>
2016-03-03 22:05:27 +0000

    minor changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true
   validates :firstname, presence: true
   validates :lastname, presence: true
-  validates :password, length: { in: 6..20}, on: :create
+  validates :password, length: { in: 5..20}, on: :create
 
   def full_name
     "#{firstname} #{lastname}"
